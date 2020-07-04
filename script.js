0ea06a21765b0fca001e44d7c575dfb2ff84d81a
romale007 <romale0819@gmail.com>
2020-07-04 13:27:12 +0300

    04072020 со старым контекстом

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 "use strict";
  
-let context = new AudioContext (); // обращаемся к глобальному аудио контексту
+
+let AudioContext = window.AudioContext || window.webkitAudioContext; // обращаемся к глобальному аудио контексту
+let audioCtx = new AudioContext();
 
 class Sound {                                     // Создаём класс , который каждый раз будет обращаться к глобальному контексту    
-    constructor(context) {
-        this.context = context;  
+    
+    constructor(audioCtx) {
+        this.audioCtx = audioCtx;  
      
     }
     init() {                                          // создаём фунцию, которая будет каждое нажатие создавать цепь осцилятор-усилитель-выход      
-        this.oscillator = context.createOscillator(); // создаём в контексте: узел осцилятора 
-        this.gainNode = context.createGain();         // создаём в контексте: узел усилителя
+        this.oscillator = audioCtx.createOscillator(); // создаём в контексте: узел осцилятора 
+        this.gainNode = audioCtx.createGain();         // создаём в контексте: узел усилителя
 
         this.oscillator.connect(this.gainNode);          // втыкаем осцилятор в усилитель
-        this.gainNode.connect(this.context.destination); // втыкаем усилитель в устройство вывода
+        this.gainNode.connect(this.audioCtx.destination); // втыкаем усилитель в устройство вывода
 
         this.oscillator.type = 'sine';                   // задаём форму осцилятора (синус самый мягкий по звучанию)
-        this.gainNode.gain.setValueAtTime(0.1, this.context.currentTime); // задаём уровень громкости (50%) и время запуска (сразу)
+        this.gainNode.gain.setValueAtTime(0.1, this.audioCtx.currentTime); // задаём уровень громкости (50%) и время запуска (сразу)
     }
     play(freq) {                                        //создаём функцию, которая будет запускать осцилятор, два параметра - частота и время запуска
         this.init();                                    // создаёт цепь  "осцилятор-усилитель-выход" и форму осцилятора 
@@ -33,25 +36,26 @@ class Sound {                                     // Создаём класс ,
 // создаём класс Нота, свойство - частота (Гц)
 
 class Note {
-    constructor(freq) {
+    constructor(freq, name) {
         this.freq = freq;
+        this.name = name;
     }
 }
 // создаём объекты - Ноты
 
 
-const C4 = new Note(261.63);
-const Db4 = new Note(277.18);
-const D4 = new Note(293.66);
-const Eb4 = new Note(311.13);
-const E4 = new Note(329.63);
-const F4 = new Note(349.23);
-const Gb4 = new Note(369.99);
-const G4 = new Note(392.00);
-const Ab4 = new Note(415.30);
-const A4 = new Note(440.00);
-const Bb4 = new Note(466.16);
-const B4 = new Note(493.88);
+const C4 = new Note(261.63, "C4");
+const Db4 = new Note(277.18, "C#4");
+const D4 = new Note(293.66, "D4");
+const Eb4 = new Note(311.13, "D#4");
+const E4 = new Note(329.63, "E4");
+const F4 = new Note(349.23, "F4");
+const Gb4 = new Note(369.99, "F#4");
+const G4 = new Note(392.00, "G4");
+const Ab4 = new Note(415.30, "G#4");
+const A4 = new Note(440.00, "A4");
+const Bb4 = new Note(466.16, "A#4");
+const B4 = new Note(493.88, "B4");
 
 
 
@@ -62,6 +66,10 @@ const B4 = new Note(493.88);
 let whiteNotesFrequencies = [C4.freq, D4.freq, E4.freq, F4.freq, G4.freq, A4.freq, B4.freq];
 
 let blackNotesFrequencies = [Db4.freq, Eb4.freq, 0, Gb4.freq, Ab4.freq, Bb4.freq];
+
+let whiteNotesNames = [C4.name, D4.name, E4.name, F4.name, G4.name, A4.name, B4.name];
+
+let blackNotesNames = [Db4.name, Eb4.name, 0, Gb4.name, Ab4.name, Bb4.name];
 
 
 //рисуем клавиши c помощью цикла
@@ -85,51 +93,44 @@ function draw() {
 
         html +='</div>';
     }
+    
     box.innerHTML = html;
-      
-    document.querySelectorAll('.whitenotes').forEach(function(element) {
-        element.onmousedown = playNow;
-        element.onmouseup = stopNow; 
-         
+
+ 
+
+
+
+document.querySelectorAll('.whitenotes').forEach(function(element) {
+    element.onmousedown = playNow;
+    element.onmouseup = stopNow;
+    element.onmouseleave = stopNow; 
+     
     });
 
       document.querySelectorAll('.blacknotes').forEach(function(element) {
-        element.onmousedown = playNow;  
-        element.onmouseup = stopNow;  
-        
-    });
-  }
-
-  let Play = new Sound(context); // создаём объект
-
-    /*
-    let doNota = document.querySelector(`.whitenotes[data="${C4.freq}"]`);
-
+        element.onmousedown = playNow;
+        element.onmouseup = stopNow;
+        element.onmouseleave = stopNow; 
+         
+    }); 
     
+}     
+ 
 
-      document.addEventListener('keypress', function(event){
-       
-        if (event.code == 'KeyQ')
-        console.log(1);
-        
-     });
-      document.addEventListener('keyup', function(event){
-        Play.stop();
-      });
-*/
-
+  let Play = new Sound(audioCtx); // создаём объект
 
 
 function playNow(){
     
-    let x = this.dataset.note;
-    Play.play(x);
-    event.stopPropagation();
-    localStorage.setItem(`currentFreq`, `${this.dataset.note}`);
+    Play.play(this.dataset.note);   // берет частоту из dataset элемента
+    event.stopPropagation();        
+    //localStorage.setItem(`currentFreq`, `${this.dataset.note}`);
 }
 
 function stopNow(){
     Play.stop();
+    
+
 }
 
 
@@ -163,7 +164,21 @@ draw();
 
 
 
+    /*
+    let doNota = document.querySelector(`.whitenotes[data="${C4.freq}"]`);
 
+    
+
+      document.addEventListener('keypress', function(event){
+       
+        if (event.code == 'KeyQ')
+        console.log(1);
+        
+     });
+      document.addEventListener('keyup', function(event){
+        Play.stop();
+      });
+*/
 
 
 
